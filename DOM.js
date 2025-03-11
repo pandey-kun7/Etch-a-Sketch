11ccb1a760852f94acde3fb6328e015fd4ac5419
Kunal Pandey <191033820+pandey-kun7@users.noreply.github.com>
2025-03-11 21:08:07 +0530

    Build the overall project

--- DOM.js
+++ DOM.js
@@ -1,22 +1,38 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    const container=document.querySelector("#container");
-    let gridsRowColumn=16
-    const button=document.querySelector(".user");
-    button.addEventListener('click',()=>{
-        gridRowColumn=parseInt(prompt("Enter the number of grids per column/row",16));
-        container.innerHTML="";
-        generateGrid(gridsRowColumn);
-    });
+var container=document.querySelector("#container");
+let gridsRowColumn=16;
+const button=document.querySelector(".user");
 
+createGrid()
+
+let colorList=["aqua", "fuchsia", "green", "lime", "maroon", "olive", "orange", "purple", "red",'yellow']
+
+function createGrid(){
     container.style.display="flex";
     container.style.flexWrap="wrap";
-    container.style.maxWidth= "350px";
+    container.style.width= "720px";
+    container.style.backgroundColor="black";
+    container.style.padding="3px";
     container.style.borderWidth="3px solid black";
-    // for(let i=0;i<gridsRowColumn;i++){
-    //     for(let j=0;j<gridsRowColumn;j++){
-    //         const smallDiv=document.createElement("div");
-    //         smallDiv.style.borderWidth="30px";
-    //         container.appendChild(smallDiv);
-    //     }
-    // }
+
+
+    let smallDivSize= (720/gridsRowColumn);
+
+    for(let i=0;i<gridsRowColumn*gridsRowColumn;i++){
+        const smallDiv=document.createElement("div");
+        smallDiv.style.borderWidth="5px solid black";
+        smallDiv.style.width=`${smallDivSize}px`;
+        smallDiv.style.height=`${smallDivSize}px`;
+        smallDiv.style.backgroundColor="white";
+        smallDiv.style.boxShadow = "inset 0px 0px 0px 1px black";
+        container.appendChild(smallDiv);
+        smallDiv.addEventListener("mouseover",()=>{
+            smallDiv.style.backgroundColor=colorList[Math.round((Math.random()*10))];
+        });
+    }
+}
+
+button.addEventListener('click',()=>{
+    container.innerHTML="";
+    gridsRowColumn=parseInt(prompt("Enter the number of grids per column/row",16));
+    createGrid();
 });
