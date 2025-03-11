715c576b9dd99b449421cc047fbd4322198a52b7
Kunal Pandey <10c.kunalpandey@gmail.com>
2025-03-11 16:22:05 +0530

    JS file Done

--- DOM.js
+++ DOM.js
@@ -1,1 +1,22 @@
-const container=document.querySelector("#container");
+document.addEventListener("DOMContentLoaded", (event) => {
+    const container=document.querySelector("#container");
+    let gridsRowColumn=16
+    const button=document.querySelector(".user");
+    button.addEventListener('click',()=>{
+        gridRowColumn=parseInt(prompt("Enter the number of grids per column/row",16));
+        container.innerHTML="";
+        generateGrid(gridsRowColumn);
+    });
+
+    container.style.display="flex";
+    container.style.flexWrap="wrap";
+    container.style.maxWidth= "350px";
+    container.style.borderWidth="3px solid black";
+    // for(let i=0;i<gridsRowColumn;i++){
+    //     for(let j=0;j<gridsRowColumn;j++){
+    //         const smallDiv=document.createElement("div");
+    //         smallDiv.style.borderWidth="30px";
+    //         container.appendChild(smallDiv);
+    //     }
+    // }
+});
